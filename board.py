bf522b6ed1ce9b1232698ad8ba3a0195f91f2a70
mguzman011 <mguzman011@gmail.com>
2016-12-04 20:20:10 -0800

    Adds pellets, eats, and grows.
    thx emily

--- board.py
+++ board.py
@@ -7,22 +7,20 @@ from visual import *
 import random
 
 def make_snek():
-    """ Builds the snake model."""
     snek = frame( pos=(0,0,0) )
     color.brown = (0.46,0.28,0.1)
-    python = sphere(frame = snek, pos=(-0.5,0,0),radius=1,color = color.brown, material=materials.BlueMarble)
-    python1 = sphere(frame = snek, pos=(0.5,0,0),radius=.6,color = color.brown, material=materials.BlueMarble)
-    eye = sphere(frame = snek, pos=(-1.2,0.42,0.4),radius=0.2,color = color.orange)
-    eye2 = sphere(frame = snek, pos=(-1.2,0.42,-0.4),radius=0.2,color = color.orange)
-    pupil = sphere(frame = snek, pos=(-1.37,0.42,0.48),radius=0.04, color = color.black)
-    pupil2 = sphere(frame = snek, pos=(-1.32,0.54,-0.48),radius=0.04,color=color.black)
-    tooth = cone(frame = snek, pos=(-1.4,-0.3,0.2),axis=(-0.02,-0.05,0),radius =0.06,color=color.white)
-    tooth2 = cone(frame = snek, pos=(-1.4,-0.3,-0.2),axis=(-0.02,-0.05,0),radius = 0.06,color=color.white)
+    python = sphere(frame = snek, pos=(-0.5,0,0), radius = 1, color = color.brown, material=materials.BlueMarble)
+    #python1 = sphere(frame = snek, pos=(0.5,0,0),radius=.6, color = color.brown, material=materials.BlueMarble)
+    #eye = sphere(frame = snek, pos=(-1.2,0.42,0.4),radius=0.2,color = color.orange)
+    #eye2 = sphere(frame = snek, pos=(-1.2,0.42,-0.4),radius=0.2,color = color.orange)
+    #pupil = sphere(frame = snek, pos=(-1.37,0.42,0.48),radius=0.04, color = color.black)
+    #pupil2 = sphere(frame = snek, pos=(-1.32,0.54,-0.48),radius=0.04,color=color.black)
+    #tooth = cone(frame = snek, pos=(-1.4,-0.3,0.2),axis=(-0.02,-0.05,0),radius =0.06,color=color.white)
+    #tooth2 = cone(frame = snek, pos=(-1.4,-0.3,-0.2),axis=(-0.02,-0.05,0),radius = 0.06,color=color.white)
     return snek
 
 def make_walls():
     """ makes several walls and returns them in a list
-        Docs here:  http://vpython.org/contents/docs/box.html
     """
     w0 = box(pos=(-20,0,0), axis=(0,0,1), 
              length=40, width=1, height = 2, color=color.red, material=materials.wood)
@@ -38,12 +36,7 @@ def make_walls():
 def main():
     """ this is the main function, including all of the data objects and the "event loop" which is the while True: loop that will be the universe's "time stream" :-)
     """
-    # first, create a new scene (not required, but doesn't hurt)
-    scene = display(title='Game',
-                        x=0, y=0, width=1000, height=1000,
-                        center=(0,0,0), background=(0,0,0), forward=(0,-2,-1), userzoom=False, userspin=False)
-    scene.select()
-    
+    scene = display(title='Game',x=0, y=0, width=1000, height=1000,center=(0,0,0), background=(0,0,0), forward=(0,-2,-1), userzoom=False, userspin=False) #camera move
     # create an object named floor of class (type) box:
     floor = box(pos=(0,-1,0), length=40, width=40, height = 0.5, color=color.white)
 
@@ -65,6 +58,12 @@ def main():
     dt = 1.0/(1.0*RATE)   # the amount of time per loop (again, if possible)
     autocenter = True     # do you want vPython to keep the scene centered?
 
+    #make a pellet
+    pelletposx = random.randint(-20, 20)
+    pelletposz = random.randint(-20,20)
+    pellet = sphere( radius=0.5, pos=(pelletposx,0,pelletposz), color = color.white )
+    bodypos = 0.5
+
     # this is the main loop of the program! it's "time" or the "event loop"
     while True:
         rate(RATE)     # run no faster than RATE loops/second
@@ -72,12 +71,20 @@ def main():
         # +++++ start of all position updates: once per loop +++++ 
 
         ball.pos = ball.pos + ball.vel*dt           # PHYSICS!
-        snek.pos = snek.pos + snek.vel*dt
+        snek.pos = snek.pos + snek.vel
 
         # +++++ end of all once-per-loop position updates +++++ 
-        
+
 
         # ----- start of other checks - especially *collisions* -----
+        vec_from_pellet_to_snek = pellet.pos-snek.pos
+        if mag(vec_from_pellet_to_snek) < 2:
+            pellet.visible = False
+            pelletposx = random.randint(-19, 19)
+            pelletposz = random.randint(-19,19)
+            pellet = sphere( radius=0.5, pos=(pelletposx,0,pelletposz), color = color.white )
+            body = sphere(frame = snek, pos=(bodypos,0,0), radius=.6 , color = color.brown, material=materials.BlueMarble)
+            bodypos += 0.5        
         
         # colliding with wall 0, w0:
         if snek.pos.x < w0.pos.x+1.5:  # w0 has the smallest x value
@@ -145,4 +152,4 @@ def main():
 
 # This should be the FINAL thing in the file...
 if __name__ == "__main__":   # did we just RUN this file?
-    main() # if so, we call main()
+    main()                   # if so, we call main()
